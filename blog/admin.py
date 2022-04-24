67c2324074657482a6826533ad635517f126f4fe
Markp1312 <mpg.peeters3@gmail.com>
2022-04-24 16:04:07 +0000

    Updated CommentAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -19,4 +19,5 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
-
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
