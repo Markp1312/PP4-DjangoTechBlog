4dacba5a95faac9d42c828ab393337ff9aaa6436
Markp1312 <mpg.peeters3@gmail.com>
2022-06-19 17:03:22 +0000

    Fixed urls with debug on

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import AddPostView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('add_post/', views.AddPostView.as_view(), name='add_post'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('add_post/', views.AddPostView.as_view(), name='add_post'),
+
 ]
